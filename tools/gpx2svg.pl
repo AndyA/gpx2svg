f45846b6101f4ed93232296693e05364d7a324c0
Andy Armstrong <andy@hexten.net>
2014-01-23 17:49:12 +0000

    Work in progress.
    Elevation sorta works.
    
    Map draws but multiple segments get piled on top of each other.

---
+++ tools/gpx2svg.pl
@@ -0,0 +1,206 @@
+#!/usr/bin/env perl
+
+use autodie;
+use strict;
+use warnings;
+
+use Data::Dumper;
+use GIS::Distance;
+use Geo::Gpx;
+use Geo::Mercator;
+use Getopt::Long;
+use List::Util qw( min max );
+use SVG;
+
+my %O = (
+  trkfile => undef,
+  elefile => undef,
+  tracks  => 1,
+  routes  => 1,
+  size    => '1000x1000',
+  vscale  => 100,
+);
+
+GetOptions(
+  't:s'      => \$O{trkfile},
+  'e:s'      => \$O{elefile},
+  'vscale:i' => \$O{vscale},
+) or syntax();
+
+unless ( defined $O{trkfile} || defined $O{elefile} ) {
+  print "Please specify one or both of\n",
+   "   -t track.svg     (generate track file)\n",
+   "or -e elevation.svg (generate elevation file)\n";
+  exit 1;
+}
+
+die "Bad page size\n" unless $O{size} =~ /^(\d+)x(\d+)$/;
+@O{ 'width', 'height' } = ( $1, $2 );
+
+my @pt = ();
+for my $fn (@ARGV) {
+  print "Loading $fn\n";
+  my $gpx = load_gpx($fn);
+  if ( $O{tracks} ) {
+    push @pt, @{ $gpx->tracks || [] };
+  }
+  if ( $O{routes} ) {
+    for my $rte ( @{ $gpx->routes || [] } ) {
+      push @pt,
+       {name     => $rte->{name},
+        segments => [{ points => $rte->{points} }],
+       };
+    }
+  }
+}
+
+if ( defined( my $trkfile = $O{trkfile} ) ) {
+  print "Drawing track\n";
+  my $svg = make_track( \@pt );
+  print "Writing track to $trkfile\n";
+  open my $of, '>', $trkfile;
+  print $of $svg->xmlify;
+}
+
+if ( defined( my $elefile = $O{elefile} ) ) {
+  print "Drawing elevation profile\n";
+  my $svg = make_profile( \@pt );
+  print "Writing elevation profile to $elefile\n";
+  open my $of, '>', $elefile;
+  print $of $svg->xmlify;
+}
+
+sub make_track {
+  my $pt = shift;
+  my $svg = SVG->new( width => $O{width}, height => $O{height} );
+  for my $leg (@$pt) {
+    print "Plotting ", $leg->{name}, "\n";
+    my ( @xv, @yv );
+    for my $seg ( @{ $leg->{segments} } ) {
+      for my $pt ( @{ $seg->{points} } ) {
+        my ( $x, $y ) = mercate( $pt->{lat}, $pt->{lon} );
+        push @xv, $x;
+        push @yv, $y;
+      }
+    }
+    scale_to_fit( $O{width}, $O{height}, \@xv, \@yv );
+    my $points = $svg->get_path(
+      x     => \@xv,
+      y     => \@yv,
+      -type => 'polyline',
+    );
+    my $tag = $svg->polyline(
+      %$points,
+      style => {
+        'fill-opacity' => 0,
+        'stroke-width' => '1',
+        'stroke'       => 'rgb(0, 0, 0)',
+      }
+    );
+  }
+  return $svg;
+}
+
+sub make_profile {
+  my $pt  = shift;
+  my $gis = GIS::Distance->new;
+
+  my @leg  = ();
+  my $dist = 0;
+  my $maxy = 0;
+  for my $leg (@$pt) {
+    print "Plotting ", $leg->{name}, "\n";
+    my ( $plat, $plon );
+    my ( $xv, $yv ) = ( [], [] );
+    my $left = $dist;
+    for my $seg ( @{ $leg->{segments} } ) {
+      for my $pt ( @{ $seg->{points} } ) {
+        $dist += $gis->distance( $plat, $plon, $pt->{lat}, $pt->{lon} )->metres
+         if defined $plat;
+        push @$xv, $dist;
+        push @$yv, $pt->{ele} * $O{vscale};
+        $maxy = $pt->{ele} if $pt->{ele} > $maxy;
+        ( $plat, $plon ) = ( $pt->{lat}, $pt->{lon} );
+      }
+    }
+    # close polygon
+    push @$xv, $xv->[-1], $left;
+    push @$yv, 0, 0;
+    push @leg, [$xv, $yv];
+  }
+
+  my $width  = $O{width};
+  my $height = $O{height};
+
+  my $scaler = make_scaler( $width, $height, 0, 0, $dist, $maxy );
+  my $svg = SVG->new( width => $width, height => $height );
+
+  for my $leg (@leg) {
+    my ( $xv, $yv ) = @$leg;
+    $scaler->( $xv, $yv );
+    #    for my $i ( 0 .. $#$xv ) {
+    #      printf "%8.3f, %8.3f\n", $xv->[$i], $yv->[$i];
+    #    }
+    my $points = $svg->get_path(
+      x       => $xv,
+      y       => $yv,
+      -type   => 'polyline',
+      -closed => 'true',
+    );
+    my $tag = $svg->polyline(
+      %$points,
+      style => {
+        'fill-opacity' => 0.5,
+        'stroke-width' => '1',
+        'stroke'       => 'rgb(0, 0, 0)',
+        'fill'         => 'rgb(0, 100, 0)',
+      }
+    );
+  }
+
+  return $svg;
+}
+
+sub bbox {
+  my ( $xv, $yv ) = @_;
+  return ( min(@$xv), min(@$yv), max(@$xv), max(@$yv) );
+}
+
+sub make_scaler {
+  my ( $ow, $oh, $minx, $miny, $maxx, $maxy ) = @_;
+  my $iw     = $maxx - $minx;
+  my $ih     = $maxy - $miny;
+  my $scale  = min( $ow / $iw, $oh / $ih );
+  my $xshift = ( $ow - $iw * $scale ) / 2;
+  my $yshift = ( $oh - $ih * $scale ) / 2;
+  return sub {
+    my ( $xv, $yv ) = @_;
+    $_ = ( $_ - $minx ) * $scale + $xshift for @$xv;
+    $_ = $oh - ( ( $_ - $miny ) * $scale + $yshift ) for @$yv;
+  };
+}
+
+sub scale_to_fit {
+  my ( $ow, $oh, $xv, $yv ) = @_;
+  make_scaler( $ow, $oh, bbox( $xv, $yv ) )->( $xv, $yv );
+}
+
+sub load_gpx {
+  my $fn = shift;
+  open my $fh, '<', $fn;
+  return Geo::Gpx->new( input => $fh );
+}
+
+sub syntax {
+  print <<EOT;
+Syntax: gpx2svg [options] <input.gpx> ...
+
+Options:
+   -t tracks.svg      Output track file
+   -e elevation.svg   Output elevation profile
+
+EOT
+  exit 1;
+}
+
+# vim:ts=2:sw=2:sts=2:et:ft=perl
