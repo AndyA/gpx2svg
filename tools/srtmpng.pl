74c392bab18583b48031b8897a64e7e1a70525b7
Andy Armstrong <andy@hexten.net>
2014-01-24 18:03:28 +0000

    Snapshot.

--- tools/srtmpng.pl
+++ tools/srtmpng.pl
@@ -27,12 +27,12 @@ use constant SCALE => 'ref/scale.png';
 #use constant LON_MIN => -20;
 #use constant LON_MAX => 20;
 
-use constant LAT_MIN => 50.5;
-use constant LAT_MAX => 65.5;
-use constant LON_MIN => -9.5;
+use constant LAT_MIN => 47.5;
+use constant LAT_MAX => 60.5;
+use constant LON_MIN => -11.5;
 use constant LON_MAX => 2.5;
 
-use constant WIDTH => 1600;
+use constant WIDTH => 1800;
 use constant HEIGHT =>
  int( WIDTH * ( LAT_MAX - LAT_MIN ) / ( LON_MAX - LON_MIN ) );
 
